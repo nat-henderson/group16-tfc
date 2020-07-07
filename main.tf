a64035f2131290112683ab84663c02651e883c20
Nathan Mckinley <nmckinley@google.com>
2020-07-07 16:22:03 -0700

    hook up subnets.

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ resource "aws_eks_cluster" "nmckinley" {
   role_arn = aws_iam_role.nmckinley.arn
 
   vpc_config {
-    subnet_ids = ["foo", "bar"]
+    subnet_ids = module.vpc.private_subnets
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
