071b845490fa7aa3601fac9fbb15799841a6774b
Nathan Mckinley <nmckinley@google.com>
2020-07-07 16:19:44 -0700

    ok i guess we need this?  destroys should not be so hard!

--- main.tf
+++ main.tf
@@ -161,4 +161,18 @@ resource "aws_iam_role_policy_attachment" "nmckinley-AmazonEKSServicePolicy" {
   role       = aws_iam_role.nmckinley.name
 }
 
+resource "aws_eks_cluster" "nmckinley" {
+  name     = "nmckinley"
+  role_arn = aws_iam_role.nmckinley.arn
 
+  vpc_config {
+    subnet_ids = ["foo", "bar"]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.nmckinley-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.nmckinley-AmazonEKSServicePolicy,
+  ]
+}
